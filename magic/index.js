65bd8852f223174a1c693f94cebfc12ab365e536
HeartAttackCat <hunter.lane@student.nmt.edu>
2024-09-18 20:16:13 -0600

    Dynamic JS code for drop down menu. Still needs CSS to buetify it.

---
+++ magic/index.js
@@ -0,0 +1,16 @@
+function hideMenu(taggy) {
+    var cusid_ele = document.getElementsByClassName(taggy);
+    for (var i = 0; i < cusid_ele.length; ++i) {
+        var item = cusid_ele[i];  
+        item.style.display = "none";
+    }
+}
+
+function showMenu(taggy) {
+    var cusid_ele = document.getElementsByClassName(taggy);
+    for (var i = 0; i < cusid_ele.length; ++i) {
+        var item = cusid_ele[i];  
+        item.style.display = "block";
+    }
+}
+
